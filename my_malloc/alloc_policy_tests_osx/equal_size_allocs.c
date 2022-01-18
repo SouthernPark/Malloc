12a298503c8d2d8f658f0be0f523083b964d158d
Southern Park <2848919007@qq.com>
2022-01-17 22:50:39 -0500

    make clean

--- my_malloc/alloc_policy_tests_osx/equal_size_allocs.c
+++ my_malloc/alloc_policy_tests_osx/equal_size_allocs.c
@@ -1,21 +1,21 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
 #include "my_malloc.h"
 
-#define NUM_ITERS    10000
-#define NUM_ITEMS    10000
-#define ALLOC_SIZE   128
+#define NUM_ITERS 10000
+#define NUM_ITEMS 10000
+#define ALLOC_SIZE 128
 
 #ifdef FF
 #define MALLOC(sz) ff_malloc(sz)
-#define FREE(p)    ff_free(p)
+#define FREE(p) ff_free(p)
 #endif
 #ifdef BF
 #define MALLOC(sz) bf_malloc(sz)
-#define FREE(p)    bf_free(p)
+#define FREE(p) bf_free(p)
 #endif
-
 
 double calc_time(struct timeval start, struct timeval end) {
   double start_sec = (double)start.tv_sec + (double)start.tv_usec / 1000000.0;
@@ -23,17 +23,16 @@ double calc_time(struct timeval start, struct timeval end) {
 
   if (end_sec < start_sec) {
     return 0;
-  } else {
+  }
+  else {
     return end_sec - start_sec;
   }
 };
 
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char * argv[]) {
   int i, j;
-  int *array[NUM_ITEMS];
-  int *spacing_array[NUM_ITEMS];
+  int * array[NUM_ITEMS];
+  int * spacing_array[NUM_ITEMS];
   unsigned long data_segment_size;
   unsigned long data_segment_free_space;
   struct timeval start_time, end_time;
@@ -41,51 +40,52 @@ int main(int argc, char *argv[])
   if (NUM_ITEMS < 10000) {
     printf("Error: NUM_ITEMS must be >= 1000\n");
     return -1;
-  } //if
+  }  //if
 
-  for (i=0; i < NUM_ITEMS; i++) {
+  for (i = 0; i < NUM_ITEMS; i++) {
     array[i] = (int *)MALLOC(ALLOC_SIZE);
     spacing_array[i] = (int *)MALLOC(ALLOC_SIZE);
-  } //for i
+  }  //for i
 
-  for (i=0; i < NUM_ITEMS; i++) {
+  for (i = 0; i < NUM_ITEMS; i++) {
     FREE(array[i]);
-  } //for i
+  }  //for i
 
   //Start Time
   gettimeofday(&start_time, NULL);
 
-  for (i=0; i < NUM_ITERS; i++) {
-    for (j=0; j < 1000; j++) {
+  for (i = 0; i < NUM_ITERS; i++) {
+    for (j = 0; j < 1000; j++) {
       array[j] = (int *)MALLOC(ALLOC_SIZE);
-    } //for j
+    }  //for j
 
-    for (j=1000; j < NUM_ITEMS; j++) {
+    for (j = 1000; j < NUM_ITEMS; j++) {
       array[j] = (int *)MALLOC(ALLOC_SIZE);
-      FREE(array[j-1000]);
+      FREE(array[j - 1000]);
 
-      if ((i==NUM_ITERS/2) && (j==NUM_ITEMS/2)) {
-	//Record fragmentation halfway through (try to repsresent steady state)
-	data_segment_size = get_data_segment_size();
-	data_segment_free_space = get_data_segment_free_space_size();
-      } //if
-    } //for j
+      if ((i == NUM_ITERS / 2) && (j == NUM_ITEMS / 2)) {
+        //Record fragmentation halfway through (try to repsresent steady state)
+        data_segment_size = get_data_segment_size();
+        data_segment_free_space = get_data_segment_free_space_size();
+      }  //if
+    }    //for j
 
-    for (j=NUM_ITEMS-1000; j < NUM_ITEMS; j++) {
+    for (j = NUM_ITEMS - 1000; j < NUM_ITEMS; j++) {
       FREE(array[j]);
-    } //for j
-  } //for i
+    }  //for j
+  }    //for i
 
   //Stop Time
   gettimeofday(&end_time, NULL);
 
   double elapsed_sec = calc_time(start_time, end_time);
   printf("Execution Time = %f seconds\n", elapsed_sec);
-  printf("Fragmentation  = %f\n", (float)data_segment_free_space/(float)data_segment_size);
+  printf("Fragmentation  = %f\n",
+         (float)data_segment_free_space / (float)data_segment_size);
 
-  for (i=0; i < NUM_ITEMS; i++) {
+  for (i = 0; i < NUM_ITEMS; i++) {
     FREE(spacing_array[i]);
   }
-  
+
   return 0;
 }
